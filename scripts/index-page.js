d8180129141c26433e15886014dfbde34ccd2a2f
Bahaa Al juboori <36934687+bahaali273@users.noreply.github.com>
2023-05-12 15:53:54 -0700

    Sprint 3 (#3)
    * Using API for index page to retrive and insirt new data
    
    * Using API for show page to retrive data to the table
    
    * update Nave bar (active and inactive)
    
    ---------
    
    Co-authored-by: Bahaa Al juboori <bahaaljuboori.Bahaali273@gmail.com>

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,12 +1,14 @@
 // // // // define your list of comments
 // // // //    -- what data is your in your list?
 
+// const { response } = require("express");
 
 
-const opinionsData = [  {    picture: "avatar",    name: "Connor Walton",    date: "02/17/2021",    comment:      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",  },
-  {    picture: "avatar",    name: "Emilie Beach",    date: "01/09/2021",    comment:      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",  },
-    {    picture: "avatar",    name: "Miles Acosta",    date: "12/20/2020",    comment:      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",  },
-  ];
+
+// const opinionsData = [  {    picture: "avatar",    name: "Connor Walton",    date: "02/17/2021",    comment:      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",  },
+//   {    picture: "avatar",    name: "Emilie Beach",    date: "01/09/2021",    comment:      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",  },
+//     {    picture: "avatar",    name: "Miles Acosta",    date: "12/20/2020",    comment:      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",  },
+//   ];
 
 const opinionsList = document.getElementById("opinions__list");
 
@@ -22,8 +24,8 @@ function createComment(comment) {
 
   let opinionsImage = "";
   // if (comment.picture === undefined) {
-  opinionsImage = document.createElement("div");
-  opinionsImage.classList.add("opinions__image--indefined");
+    opinionsImage = document.createElement("div");
+    opinionsImage.classList.add("opinions__image--indefined");
   // } else {
   //   opinionsImage = document.createElement("img");
   //   opinionsImage.classList.add("opinions__image");
@@ -39,8 +41,11 @@ function createComment(comment) {
   opinionsName.innerText = comment.name;
   opinionsName.classList.add("opinions__name");
 
+  // const opinionsDate = document.createElement("p");
+  // opinionsDate.innerText = comment.date;
+  // opinionsDate.classList.add("opinions__date");
   const opinionsDate = document.createElement("p");
-  opinionsDate.innerText = comment.date;
+  opinionsDate.innerText = new Date(comment.timestamp).toLocaleDateString();
   opinionsDate.classList.add("opinions__date");
 
   const opinionsText = document.createElement("p");
@@ -57,6 +62,45 @@ function createComment(comment) {
   opinionsItem.appendChild(opinionsBox1);
   opinionsList.appendChild(opinionsItem);
 }
+
+const apiKey = `e0eea5f0-0f8c-4b54-9fc4-ff50843766d4`;
+const getRequest ="comments";
+const commentsURL =`https://project-1-api.herokuapp.com`;
+
+let opinionsData =[];
+function getAPIData(){
+  axios
+  .get(`${commentsURL}/${getRequest}?api_key=${apiKey}`)
+  .then((response)=>{
+    opinionsData=response.data;
+    // call back data
+    let newData = opinionsData.sort(
+      (a, b) => {
+        // console.log("a:",a.timestamp)
+        // console.log("b:",b.timestamp)
+        return b.timestamp - a.timestamp
+        
+      }
+    );
+
+    console.log(opinionsData);
+    console.log(newData);
+    opinionsData.forEach((comment)=>{
+      displayComments(
+        comment.comment,
+        comment.name,
+        comment.timpestamp,
+        opinionsList
+      );
+     // console.log(opinionsData);// it dose not work ?!
+
+    });
+  })
+  .catch((error) => {
+    console.log("error: ", error);
+  });
+}
+getAPIData();
 
 function displayComments() {
   opinionsList.innerText = "";
@@ -79,11 +123,22 @@ function handleSubmit(event) {
   const newEntry = {
     name: event.target.fullName.value,
     comment: event.target.yourComment.value,
-    date: `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`,
+   // date: `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`,
   };
 
-  opinionsData.push(newEntry);
-  displayComments();
-
+  axios
+  .post(`${commentsURL}/${getRequest}?api_key=${apiKey}`,newEntry)
+  .then((response)=>{
+    console.log(response);
+    getAPIData();
+    displayComments();
+  })
+  .catch((error) => {
+   console.log("error: ",error);
+  });
   event.target.reset();
+  // opinionsData.push(newEntry);
+  // displayComments();
+
+  // event.target.reset();
 }
